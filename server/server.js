c20f2783d5db4425285a8cf626c86a24d361d201
SleekWingX <alexisyazzie365@gmail.com>
2024-08-01 19:59:38 -0600

    added server code to run mongodb

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables from .env file
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -12,6 +14,19 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+});
+
+// MongoDB connection URI
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/default_db';
+
+// Connect to MongoDB
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
@@ -36,7 +51,7 @@ const startApolloServer = async () => {
     });
   }
 
-  db.once('open', () => {
+  mongoose.connection.once('open', () => { // Ensure the DB is open before starting the server
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
